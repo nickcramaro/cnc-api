102167ed60733f2863576ba42c273696ab98b772
Nick Cramaro <nickcramaro@gmail.com>
2020-10-21 21:25:53 -0400

    Config change

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'cnc_api'),
+        database: env('DATABASE_NAME', 'cnc'),
         username: env('DATABASE_USERNAME', 'cnc_user'),
         password: env('DATABASE_PASSWORD', 'greenlife'),
         ssl: env.bool('DATABASE_SSL', false),
